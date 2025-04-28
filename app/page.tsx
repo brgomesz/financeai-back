670fb13fcd0c9bf1acb8cf3ed82148737440fab3
Bruno Rodrigues <bruno.rodrigues.gomes@hotmail.com>
2025-04-28 11:17:41 -0300

    Adicionando autenticação com clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "./_components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { dark } from "@clerk/themes";
 
-function Home() {
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return (
-    <Button>Ola mundo</Button>
-  )
-}
+    <div className="h-full flex items-center justify-center">
+      <UserButton
+        showName
+      />
+    </div>
+  );
+};
 
 export default Home;
